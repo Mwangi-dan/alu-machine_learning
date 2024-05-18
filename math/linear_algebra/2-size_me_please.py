ae2e355029d54db4946ae887db8466cdde3492ba
mwangi_dan <d.ndungu@alustudent.com>
2024-05-18 14:11:37 +0000

    Size me please

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,9 +5,13 @@ Function that calculates the shape of a matrix
 
 
 def matrix_shape(matrix):
+    n_matrix = matrix
     shape = []
-    for i in matrix:
-        shape.append(len(i))
-    shape = list(set(shape))
-    shape.append(len(matrix))
+    while isinstance(n_matrix, list):
+        shape.append(len(n_matrix))
+        if len(n_matrix) > 0:
+            n_matrix = n_matrix[0]
+        else:
+            break
+
     return shape
