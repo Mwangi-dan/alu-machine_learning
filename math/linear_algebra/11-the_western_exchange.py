2531eb44b861f3dfdc990aa25e314389f08acc23
mwangi_dan <d.ndungu@alustudent.com>
2024-05-19 18:20:14 +0000

    The western exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Matrix transposition
+'''
+
+
+def np_transpose(matrix):
+    '''
+    Returns
+    t_matrix: (list) transposed matrix
+    '''
+    return matrix.transpose()
