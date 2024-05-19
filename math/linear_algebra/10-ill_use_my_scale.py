97838a9d2cd18929a2509a596058382d103ec004
mwangi_dan <d.ndungu@alustudent.com>
2024-05-19 18:16:23 +0000

    I'll use my scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Function that calculates shape og numpy.ndarray
+'''
+
+
+def np_shape(matrix):
+    '''
+    Returns:
+    (x): tuple - shape of array
+    '''
+    return tuple(matrix.shape)
