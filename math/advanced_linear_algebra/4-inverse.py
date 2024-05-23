957d374e9a18184ab00a19d5d6190f5d2cf63ffa
mwangi_dan <d.ndungu@alustudent.com>
2024-05-23 21:25:10 +0000

    Inverse

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -8,11 +8,11 @@ def inverse(matrix):
     '''
     Returns:
     inv: (list) inverse of matrix
-    ''' 
+    '''
     adj = __import__('3-adjugate').adjugate
     det = __import__('0-determinant').determinant
 
     det_matrix = det(matrix)
     adj_matrix = adj(matrix)
 
-    return 1/det_matrix * adj_matrix
+    return (1/det_matrix) * adj_matrix
