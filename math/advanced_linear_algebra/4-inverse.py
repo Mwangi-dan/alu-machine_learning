7e4ad8584e92e0c9dbc31232b89a0b9b12ea872b
mwangi_dan <d.ndungu@alustudent.com>
2024-05-23 21:42:11 +0000

    Inverse

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -9,6 +9,15 @@ def inverse(matrix):
     Returns:
     inv: (list) inverse of matrix
     '''
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a list of lists')
+    height = len(matrix)
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError('matrix must be a list of lists')
+        elif len(row) != height or len(row) == height and len(row) == 0:
+            raise ValueError('matrix must be a non-empty square matrix')
+
     det = __import__('0-determinant').determinant
     adj = __import__('3-adjugate').adjugate
 
