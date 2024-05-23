c6d04c1757c87e0f20d68b37f59d363f14ea1b96
mwangi_dan <d.ndungu@alustudent.com>
2024-05-23 21:16:18 +0000

    adjugate

---
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+Module with fucntion to get adjugate
+'''
+
+
+def adjugate(matrix):
+    '''
+    Returns:
+    adj: (list) transposed cofactor matrix
+    '''
+    cofactor = __import__.('2-cofactor').cofactor
+
+    cof = cofactor(matrix)
+
+    rows = len(cof)
+    cols = len(cof[0])
+
+    adj = []
+
+    for col in range(cols):
+        new_row = []
+        for row in range(rows):
+            new_row.append(cof[row][col])
+        adj.append(new_row)
+
+    return adj
