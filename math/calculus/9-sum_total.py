ce1f8fa4169520de42d4dad9eef6ab2b8c5c09f9
Mwangi-dan <d.ndungu@alustudent.com>
2024-06-01 16:08:47 +0300

    Summation py

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Summation function
+"""
+
+
+def summation_i_squared(n):
+    """
+    n: (int)
+    Returns:
+    sum: (int) summation
+    """
+    sum = 0
+    for i in range(1, n):
+        sum += i * i
+    return sum
