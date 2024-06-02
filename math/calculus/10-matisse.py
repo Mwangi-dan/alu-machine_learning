052c7f06268abfa8a39be597d2034adf7f92fa7a
Mwangi-dan <d.ndungu@alustudent.com>
2024-06-02 11:41:01 +0300

    Integral and derivative

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Calculating derivative of
+a polynomial
+"""
+
+
+def poly_derivative(poly):
+    """
+    calculates the derivative of a polynomial
+    """
+    if not isinstance(poly, list) or len(poly) == 1:
+        return None
+    elif len(poly) == 1:
+        return [0]
+
+    derivative = [poly[i] * i for i in range(1, len(poly))]
+    if len(derivative) == 0:
+        return None
+
+    if all(x == 0 for x in derivative):
+        return [0]
+
+    return derivative
---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Summation function
+"""
+
+
+def summation_i_squared(n):
+    """
+    n: (int)
+    Returns
+    sum: (int) Summation
+    """
+    sum = 0
+    for i in range(1, n):
+        sum += i * i
+    return sum
