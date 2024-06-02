fdf4ca3bbfa2d2b746ba5f0a7f4be44e53690aaa
Mwangi-dan <d.ndungu@alustudent.com>
2024-06-02 11:45:34 +0300

    Integral and derivative

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -9,12 +9,13 @@ def poly_derivative(poly):
     """
     calculates the derivative of a polynomial
     """
-    if not isinstance(poly, list) or len(poly) == 1:
+    if not isinstance(poly, list):
         return None
     if len(poly) == 1:
         return [0]
 
     derivative = [poly[i] * i for i in range(1, len(poly))]
+
     if len(derivative) == 0:
         return None
 
