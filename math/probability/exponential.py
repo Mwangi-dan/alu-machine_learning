ca4c628b8c5da68482ea25e3de5cf52cc671ebeb
Mwangi-dan <d.ndungu@alustudent.com>
2024-06-09 18:09:37 +0300

    Exponential

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -38,7 +38,7 @@ class Exponential:
         Returns:
         PDF Value of x
         """
-        if x <= 0:
+        if x < 0:
             return 0
         return self.lambtha * (e ** (-self.lambtha * x))
 
@@ -49,4 +49,6 @@ class Exponential:
         Returns:
         CDF Value of x
         """
+        if x < 0:
+            return 0
         return 1 - (e ** (-self.lambtha * x))
