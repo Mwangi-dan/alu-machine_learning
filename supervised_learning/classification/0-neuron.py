a9c682627f8372ce638575c28bc1ab5374d44eb0
Mwangi-dan <d.ndungu@alustudent.com>
2024-09-12 18:30:43 +0300

    used np to instatiate W

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -24,6 +24,6 @@ class Neuron:
             print("nx must be a positive integer")
 
         self.nx = nx
-        self.W = np.random.normal()
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
