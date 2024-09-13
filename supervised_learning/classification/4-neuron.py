d17b4b4e702bff9df0225754cf949075d285579c
Mwangi-dan <d.ndungu@alustudent.com>
2024-09-13 21:22:57 +0300

    Evaluate Neuron

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+Module that holds the class of 'Neuron'
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """
+    class that defines a single Neuron performinh binary classification
+    """
+    def __init__(self, nx):
+        """
+        Constructor
+        """
+        try:
+            if type(nx) is not int:
+                raise TypeError("nx must be an integer")
+            elif nx < 1:
+                raise ValueError("nx must be a positive integer")
+        except TypeError:
+            raise TypeError("nx must be an integer")
+        except ValueError:
+            raise ValueError("nx must be a positive integer")
+
+        self.nx = nx
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    # getter function - W
+    @property
+    def W(self):
+        """
+        Getter function for W
+        """
+        return self.__W
+
+    # getter function - b
+    @property
+    def b(self):
+        """
+        Getter function for b
+        """
+        return self.__b
+
+    # getter function - A
+    @property
+    def A(self):
+        """
+        Getter function for A
+        """
+        return self.__A
+
+    def forward_prop(self, x):
+        """
+        Method that calculates the forward propagation of the neuron
+        """
+        self.__A = 1 / (1 + np.exp(-(np.matmul(self.__W, x) + self.__b)))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Method that calculates the cost of the model using logistic regression
+        """
+        m = Y.shape[1]
+        cost = -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))) / m
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Method that evaluates the neuron's predictions
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
