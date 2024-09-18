fe3d07c39eab571e81c15b52eb289ada2409ed1f
Mwangi-dan <d.ndungu@alustudent.com>
2024-09-19 00:48:10 +0300

    Forward propagation

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -8,7 +8,6 @@ import tensorflow as tf
 create_layer = __import__('1-create_layer').create_layer
 
 
-
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
     Creates forward propagation graph for NN
@@ -16,7 +15,7 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     layer_sizes: list with number of nodes in each layer
     activations: list with activation functions for each layer of network
 
-    Returns: prediction of network in tensor form 
+    Returns: prediction of network in tensor form
     """
     for i in range(len(layer_sizes)):
         if i == 0:
