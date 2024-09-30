8403e337057441935a7aad6a8ad72d4dccfd1e81
Mwangi-dan <d.ndungu@alustudent.com>
2024-09-30 22:46:07 +0300

    L2 Regularization Cost

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -3,7 +3,7 @@
 L2 Regularization Cost
 """
 
-import numpy as np
+import tensorflow as tf
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
@@ -20,5 +20,5 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     norm = 0
     for i in range(1, L + 1):
-        norm += np.linalg.norm(weights['W' + str(i)])
+        norm += tf.nn.l2_loss(weights['W' + str(i)])
     return cost + (lambtha / (2 * m)) * norm
