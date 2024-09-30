64cd0e59a6817b78fd96e0cfe1d3db43c1ae81ce
Mwangi-dan <d.ndungu@alustudent.com>
2024-09-30 23:04:24 +0300

    L2 Regularization Cost

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,20 +6,11 @@ L2 Regularization Cost
 import tensorflow as tf
 
 
-def l2_reg_cost(cost, lambtha, weights, L, m):
+def l2_reg_cost(cost):
     """
-    Calculates the cost of a neural network with L2 regularization
+    Cost: tensor containing the cost of the network
+    without L2 regularization
 
-    cost: cost of the network without L2 regularization
-    lambtha: regularization parameter
-    weights: dictionary of the weights and biases
-    L: number of layers in the neural network
-    m: number of data points used
-
-    Returns: a tensor containing the cost of the
-    network accounting for L2 regularization
+    Returns: tensor containing the cost of the network
     """
-    norm = 0
-    for i in range(1, L + 1):
-        norm += tf.nn.l2_loss(weights['W' + str(i)])
-    return cost + (lambtha / (2 * m)) * norm
+    return cost + tf.losses.get_regularization_losses()
