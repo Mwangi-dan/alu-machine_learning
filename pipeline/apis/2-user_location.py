72ebf47edeb1262621995bc18e0c72583c3b4e61
Mwangi-dan <d.ndungu@alustudent.com>
2024-11-02 16:43:14 +0300

    Rate me if you can

---
+++ pipeline/apis/2-user_location.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Prints ths location of a specific user
+"""
+import requests
+import time
+from datetime import datetime
+
+
+def userLocation(url):
+    """
+    url: User id passed as first argument of the script
+
+    Returns: the location of a specified user
+    """
+    response = requests.get(url)
+
+    if response.status_code == 404:
+        print("Not Found")
+    elif response.status_code == 403:
+        reset_time = int(response.headers['X-Ratelimit-Reset'])
+        current_time = int(time.time())
+        reset_in_minutes = int((reset_time - current_time) / 60)
+        print("Reset in {} min".format(reset_in_minutes))
+    else:
+        user = response.json()
+        location = user.get('location')
+        print(location)
+
+
+if __name__ == "__main__":
+    import sys
+
+    userLocation(sys.argv[1])
