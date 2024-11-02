29f9ede1ee766a9e729756cf65156f53d547df66
Mwangi-dan <d.ndungu@alustudent.com>
2024-11-02 16:46:49 +0300

    Rate me if you can

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -7,7 +7,7 @@ import time
 from datetime import datetime
 
 
-def userLocation(url):
+def main(url):
     """
     url: User id passed as first argument of the script
 
@@ -16,19 +16,17 @@ def userLocation(url):
     response = requests.get(url)
 
     if response.status_code == 404:
-        print("Not Found")
+        print("Not found")
     elif response.status_code == 403:
-        reset_time = int(response.headers['X-Ratelimit-Reset'])
-        current_time = int(time.time())
-        reset_in_minutes = int((reset_time - current_time) / 60)
+        reset_timestamp = int(response.headers["X-Ratelimit-Reset"])
+        current_timestamp = int(time.time())
+        reset_in_minutes = (reset_timestamp - current_timestamp) // 60
         print("Reset in {} min".format(reset_in_minutes))
     else:
-        user = response.json()
-        location = user.get('location')
-        print(location)
+        print(response.json()["location"])
 
 
 if __name__ == "__main__":
     import sys
 
-    userLocation(sys.argv[1])
+    main(sys.argv[1])
