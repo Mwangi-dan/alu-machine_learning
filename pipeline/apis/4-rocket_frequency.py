9bc27443e41e0fe4f1293f401cc15c63a755589e
Mwangi-dan <d.ndungu@alustudent.com>
2024-11-02 17:42:07 +0300

    Rocket frequency

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -3,30 +3,35 @@
 Displays number of launches per rocket
 """
 import requests
+from collections import Counter
 
 
 def rocket_frequency():
     """
     Returns the number of launches per rocket
     """
-    url = 'https://api.spacexdata.com/v4/rockets'
+    url = "https://api.spacexdata.com/v4/launches"
+    response = requests.get(url)
+    launches = response.json()
+
+    # Count the launches per rocket ID
+    rocket_counts = Counter(launch["rocket"] for launch in launches)
+
+    # Get rocket names
+    url = "https://api.spacexdata.com/v4/rockets"
     response = requests.get(url)
     rockets = response.json()
+    rocket_names = {rocket["id"]: rocket["name"] for rocket in rockets}
 
-    # Create a dictionary to store the number of launches per rocket
-    rocket_frequency = {}
-    for rocket in rockets:
-        rocket_id = rocket.get('id')
-        rocket_name = rocket.get('name')
-        launches_url = 'https://api.spacexdata.com/v4/launches'
-        response = requests.get(launches_url)
-        launches = response.json()
-        launches_per_rocket = 0
-        for launch in launches:
-            if launch.get('rocket') == rocket_id:
-                launches_per_rocket += 1
-        rocket_frequency[rocket_name] = launches_per_rocket
-    return rocket_frequency
+    # Create a list of (rocket_name, count) and sort it
+    rocket_launch_counts = [
+        (
+            rocket_names[rocket_id], count
+        ) for rocket_id, count in rocket_counts.items()
+    ]
+    rocket_launch_counts.sort(key=lambda x: (-x[1], x[0]))
+
+    return rocket_launch_counts
 
 
 if __name__ == "__main__":
